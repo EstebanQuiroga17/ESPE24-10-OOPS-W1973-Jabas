798e6dc4de79ae59d6127b7df2ea88d3bf166bc0
Esteban Quiroga <quirogaesteban1@gmail.com>
2024-12-21 18:39:09 -0500

    Dashboard class completed

---
+++ 06-Code/SureFinventory/src/ec/edu/espe/surefinventory/view/SureFinventoryApp.java
@@ -0,0 +1,18 @@
+
+package ec.edu.espe.surefinventory.view;
+
+import ec.edu.espe.surefinventory.model.DashBoard;
+
+/**
+ *
+ * @author Esteban Quiroga 
+ */
+public class SureFinventoryApp {
+    public static void main(String[] args) {
+        DashBoard dashboard = new DashBoard();
+        
+        dashboard.printLogInDashBoard();
+        
+        
+    }
+}
