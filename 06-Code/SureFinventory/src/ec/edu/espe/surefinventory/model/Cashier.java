9f7a195005ea75f4bf686c0d93574de812c2a4b2
Abnxr <abnerfabian147@gmail.com>
2024-12-21 18:05:19 -0500

    Cashier

--- 06-Code/SureFinventory/src/ec/edu/espe/surefinventory/model/Cashier.java
+++ 06-Code/SureFinventory/src/ec/edu/espe/surefinventory/model/Cashier.java
@@ -1,45 +1,96 @@
-package ec.edu.espe.surefinventory.model;
+
+package sureinventoryapp.model;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
 
 /**
  *
- * @author Matias Rojas
+ * @author abner
  */
 public class Cashier {
     private String username;
     private String password;
+    private int id;
+
+    public Cashier(String username, String password, int id) {
+        this.username = username;
+        this.password = password;
+        this.id = id;
+    }
 
     @Override
     public String toString() {
-        return "Cashier{" + "username=" + username +
-                ", password=" + password +
-                "\n";
+        return String.format(
+            "%-10s | %-15s | %-10s%n" +
+            "---------------------------------%n" +
+            "%-10d | %-15s | %-10s",
+            "ID", "Username", "Password",
+            id, username, password,
+            "\n"
+        );
+    }
+    
+    
+    
+    public static boolean logIn(String inputUsername, String inputPassword, String jsonFilePath){
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(new File(jsonFilePath));
+            String jsonUsername = rootNode.get("userName").asText();
+            String jsonPassword = rootNode.get("password").asText();
+            
+            return inputUsername.equals(jsonUsername) && inputPassword.equals(jsonPassword);
+            
+        }catch (IOException e) {
+        System.err.println("Error reading JSON file: " + e.getMessage());
+        }
+          return false;         
+        }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
     }
 
-    public Cashier(String username,String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public void closeCashRegister() {
-        System.out.println("Cash register closed by " + username);
-    }
-
-    // Getters and setters 
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
+    /**
+     * @return the password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param password the password to set
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-}
 
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+
+}
