2171cca42481b8146e66403ba8dc5041e2b0a0a7
Abnxr <abnerfabian147@gmail.com>
2024-12-24 13:03:11 -0500

    Change Manager

--- 06-Code/SureFinventory/src/ec/edu/espe/surefinventory/model/Manager.java
+++ 06-Code/SureFinventory/src/ec/edu/espe/surefinventory/model/Manager.java
@@ -1,13 +1,11 @@
-package ec.edu.espe.surefinventory.model;
 
-import java.util.Scanner;
-import ec.edu.espe.surefinventory.utils.*;
-import java.util.ArrayList;
-import java.util.Calendar;
+package ec.edu.espe.sureinveoryapp.model;
+
+import ec.edu.espe.sureinveoryapp.utils.JsonFileManager;
 
 /**
  *
- * @author Matias Rojas
+ * @author abner
  */
 public class Manager {
     private String username;
@@ -17,53 +15,65 @@ public class Manager {
         this.username = username;
         this.password = password;
     }
-    
 
     @Override
     public String toString() {
         return "Manager{" + "username=" + username + ", password=" + password + '}';
     }
     
-    public void changeMenu(Menu menu){
+    public void change(Menu menu){
         System.out.println("El menú ha sido actualizado: " + menu.toMenuString());
     }
     
-    public Order takeOrder(Customer customer, ArrayList<Dish> dishes){
+      public Order takeOrder(Customer customer, ArrayList<Dish> dishes){
         Order order = new Order(customer, dishes);
         System.out.println("Orden tomada del cliente: " + customer.getName());
         return order;
         
     }
-    
-    public Cashier createCashier(String username, String password){
+      
+      public Cashier createCashier(String username, String password){
         Cashier cashier = new Cashier(username, password);
         System.out.println("Nuevo cajero creado con username: " + username);
         return cashier;    
     }
-    
-    public void logIn(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el nombre de usuario: ");
-        String inputUsername = scanner.nextLine();
-        System.out.print("Ingrese la contraseña: ");
-        String inputPassword = scanner.nextLine();
-
-        if (this.username.equals(inputUsername) && this.password.equals(inputPassword)) {
-            System.out.println("Inicio de sesión exitoso. Bienvenido " + username + ".");
-        } else {
-            System.out.println("Nombre de usuario o contraseña incorrectos.");
+      
+      public static boolean logIn(){
+        String filePath = "C:/Users/abner/Documents/ESPE2410-OOPSW1973-JABAS/06-Code/SureFinventory/data/admin.json";
+        JsonFileManager jsonFileManager = new JsonFileManager(filePath);
+        
+        ArrayList<Cashier> cashiers = jsonFileManager.decerializeJson(Cashier.class);
+        
+        if (cashiers == null || cashiers.isEmpty()) {
+            System.out.println(" There is no data in the JSON file ");
+            return false;
         }
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your username ");
+        String inputUsername = scanner.nextLine();
+        System.out.print("Enter your password: ");
+        String inputPassword = scanner.nextLine();
+        
+        for (Cashier cashier : cashiers) {
+            if (cashier.getUsername().equals(inputUsername) && cashier.getPassword().equals(inputPassword)) {
+                System.out.println("Login successful. WELCOME! " + inputUsername + "!");
+                return true;
+            }
+        }
+        
+        System.out.println("INCORRECT ---> check username and password");
+        return false;
     }
-    
-    public void cancelOrder(Order order){
+      public void cancelOrder(Order order){
         System.out.println("La orden con ID " + order.getId() + " ha sido cancelada.");
     }
-    
-    public void addExpense(Expense expense){
+      public void addExpense(Expense expense){
         System.out.println("Se ha añadido un gasto: " + expense.toString());
-    }
-    
-    public AccountingReport createAccountingReport(Calendar fromDate, Calendar toDate){
+      }
+      
+      public AccountingReport createAccountingReport(Calendar fromDate, Calendar toDate){
         ArrayList<Invoice> incomes = new ArrayList<>(); 
         ArrayList<Expense> expenses = new ArrayList<>(); 
         incomes.add(new Invoice(1000, fromDate));
@@ -73,22 +83,7 @@ public class Manager {
         System.out.println("Informe contable creado desde " + fromDate.getTime() + " hasta " + toDate.getTime());
         return report;
     }
-    
-    
-
-    /**
-     * @return the username
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * @param username the username to set
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
+      
 
     /**
      * @return the password
@@ -98,11 +93,27 @@ public class Manager {
     }
 
     /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
      * @param password the password to set
      */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
     
+    
+    
+
 }
