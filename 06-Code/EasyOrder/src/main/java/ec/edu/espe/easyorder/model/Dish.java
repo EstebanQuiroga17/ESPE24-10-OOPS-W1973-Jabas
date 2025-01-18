a82b02d0ad568ecebeee31e26843b8fdc356577e
Abnxr <abnerfabian147@gmail.com>
2025-01-18 16:36:23 -0500

    Dish uptade

--- 06-Code/EasyOrder/src/main/java/ec/edu/espe/easyorder/model/Dish.java
+++ 06-Code/EasyOrder/src/main/java/ec/edu/espe/easyorder/model/Dish.java
@@ -2,8 +2,68 @@ package ec.edu.espe.easyorder.model;
 
 /**
  *
- * @author Matias Rojas
+ * @author Abner Proaño
  */
 public class Dish {
+    private String name;
+    private int id;
+    private float price;
+
+    public Dish(String name, int id, float price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" + "name=" + name + ", id=" + id + ", price=" + price + '}';
+    }
+    
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    
+    
     
 }
