f023f89b351e7d01410ae9fcd2779330c6389093
rojasmatias18 <mati2006rojas@gmail.com>
2025-01-22 23:14:42 -0500

    Frsm addCustomer changes

--- 06-Code/EasyOrder/src/main/java/ec/edu/espe/easyorder/view/FrmCustomer.java
+++ 06-Code/EasyOrder/src/main/java/ec/edu/espe/easyorder/view/FrmCustomer.java
@@ -137,6 +137,7 @@ public class FrmCustomer extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(FrmCustomer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
